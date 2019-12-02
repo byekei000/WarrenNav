eda80d6d216d00067f41bb8a3c4e596747eb49be
kjbye5252 <kjbye5252@gmail.com>
2019-12-01 19:37:03 -0500

    changed port

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
 require('leaflet-geometryutil');
 require('leaflet-snap');
 require('leaflet-routing-machine');
